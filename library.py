2a241be92e0fe3ba47d3597b452a7ef4b8ab49d8
umarfarooqoq90368-prog <umarfarooqoq90368@gmail.com>
2026-03-06 20:44:46 +0500

    today
    solving

--- library.py
+++ library.py
@@ -1,2 +1,38 @@
+from secrets import choice
+
 import numpy as np
 import pandas as pd
+dic={"name":[],"age":[],"balance":[]}
+df=pd.DataFrame(dic)
+print(df)
+
+"""balance=0
+while True:
+    method = input("Enter your choice:"
+                   "\n 1:create account: "
+                   "\n 2:check  balance:"
+                   "\n 3:withDraw amount"
+                   "\n 4:Deposit amount"
+                   )
+    choice = input("Enter your choice (y/n")
+
+    if method=="1":
+        name=input("Enter your name:")
+        age=int(input("Enter your age:"))
+    elif name==dic["name"]:
+        print(f"{name}your account is already created"{balance}"")
+        print(f"{name}account created and your balance is: {balance}:")
+        print(choice=="y")
+        continue
+    if method=="2":
+        print(f"{balance}")
+    if method=="3":
+        amount=int(input("Enter your amount:"))
+        balance=balance-amount
+        print(f"your current balance is: {balance}")
+    if method=="4":
+        amount=int(input("Enter your amount:"))
+        balance=balance+amount
+        print(f"your current balance is: {balance}")"""
+
+
--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 #1 The Multi-Line Story:
+print("""A boy found an old key in his garden
+He tried it on a locked box in his house.
+Inside, he discovered his grandfather’s hidden treasure. 
+""")
 
 #2The Border:
-"""print("*python*")
+print("*********")
+print(" Python ")
+print("*********")
+
 #3The Separator:
-print(1,2,3,4,5,sep="->")
+print(1,2,3,4,5,sep=" ->")
 #4 The End Game:
+
 print("my name is umar",end=" ")
 print("I am student")
+
 #5. The Formatter:
 decnum=12.34567
-print(f"{decnum} is {decnum}")
-
+print(f"{decnum:.2f}")
 
 #2. The input() Function
 #1 The Greeter
-name=input("enter your name")
-color=input("enter your color")
-print(f"hi {name} is {color} looks great on you")
+name = input("Enter your name: ")
+color = input("Enter your favorite color: ")
+print(f"Hi {name}, {color} looks great on you")
 
 #2 The Age Gap:
 age=int(input("enter your age"))
-print(f"{age+50} your age in 2050")
+current_year=int(input("enter your current year"))
+future_age=age+(2050-current_year)
+print(f"In 2050 you will be {future_age} years old")
 
 #3 The Multiplier:
 num1=int(input("enter your first number"))
 num2=int(input("enter your second number"))
 print(num1*num2)
-TypeError: can't multiply sequence by non-int of type 'str'
+#TypeError: can't multiply sequence by non-int of type 'str'
 #4 The Repeater:
 word=input("enter your word")
 num=int(input("enter your number"))
@@ -41,19 +51,27 @@ print(f"{totalbill/people}")
 # 1The Remainder:
 num1=int(input("enter your first number"))
 num2=int(input("enter your second number"))
-print(f"number one:{num1} and number two: {num2}and its Remainder:{num1/num2} ")
+print(f"number one:{num1} and number two: {num2}and its Remainder:{num1%num2} ")
+
 #2. The Power:
 x=int(input("enter your first number"))
 y=int(input("enter your second number"))
 print(f"{x**y}")
+
 #3. The Floor:
 num1=int(input("enter your first number"))
 num2=int(input("enter your second number"))
 print(f"{num1//num2}")
+
 #4. The Comparison:
+num = int(input("Enter a number: "))
+print(num > 100)
 
-name=input("enter your name")
-color=input("enter your color")
-print(f"hi {name} is {color} looks great on you")"""
+#5.The Swapper:
+a=10
+b=15
+a,b=a,b
+print("a=",a)
+print("b=",b)
 
-print(10-3*2)
+

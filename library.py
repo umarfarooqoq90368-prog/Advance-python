2f47dc70a03248ba5e62931b7f1f09994387e96a
umarfarooqoq90368-prog <umarfarooqoq90368@gmail.com>
2026-03-11 12:22:18 +0500

    all
    all files

--- library.py
+++ library.py
@@ -2,11 +2,7 @@ from secrets import choice
 
 import numpy as np
 import pandas as pd
-dic={"name":[],"age":[],"balance":[]}
-df=pd.DataFrame(dic)
-print(df)
-
-"""balance=0
+dic={"name":[],"age":[],"balance":[5000,]}
 while True:
     method = input("Enter your choice:"
                    "\n 1:create account: "
@@ -14,25 +10,43 @@ while True:
                    "\n 3:withDraw amount"
                    "\n 4:Deposit amount"
                    )
-    choice = input("Enter your choice (y/n")
+    choice = input("Enter your choice (y/n): ")
 
     if method=="1":
         name=input("Enter your name:")
         age=int(input("Enter your age:"))
-    elif name==dic["name"]:
-        print(f"{name}your account is already created"{balance}"")
-        print(f"{name}account created and your balance is: {balance}:")
+        dic["name"].append(name)
+        dic["age"].append(age)
+        dic["balance"].append("balance")
+        print(f"{name}account created and your balance is: {"balance"}:")
+        for i in dic:
+            if i=="name":
+                dic[i]=choice
+        print(f"{name}your account is already created{"balance"}")
         print(choice=="y")
         continue
     if method=="2":
-        print(f"{balance}")
+        print(f"{"balance"}")
+        for d in dic:
+            if dic[d]=="balance":
+                dic[d]=choice
+                print(f"{name}your  current account {"balance"}")
     if method=="3":
         amount=int(input("Enter your amount:"))
-        balance=balance-amount
-        print(f"your current balance is: {balance}")
+        for f in dic:
+            if dic[f] == "balance":
+                dic[f]=choice
+        balance="balance"-amount
+        dic["balance"].append("balance")
+        print(f"your current balance is: {"balance"}")
     if method=="4":
+        name=input("Enter your name:")
         amount=int(input("Enter your amount:"))
+        for h in dic:
+            if dic[h] == name:
+                dic[h]=choice
         balance=balance+amount
-        print(f"your current balance is: {balance}")"""
+        dic["balance"].append(balance)
+        print(f"your current balance is: {"balance"}")
 
 
--- main.py
+++ main.py
@@ -1,77 +1,24 @@
-#1 The Multi-Line Story:
-print("""A boy found an old key in his garden
-He tried it on a locked box in his house.
-Inside, he discovered his grandfather’s hidden treasure. 
-""")
-
-#2The Border:
-print("*********")
-print(" Python ")
-print("*********")
-
-#3The Separator:
-print(1,2,3,4,5,sep=" ->")
-#4 The End Game:
-
-print("my name is umar",end=" ")
-print("I am student")
-
-#5. The Formatter:
-decnum=12.34567
-print(f"{decnum:.2f}")
-
-#2. The input() Function
-#1 The Greeter
-name = input("Enter your name: ")
-color = input("Enter your favorite color: ")
-print(f"Hi {name}, {color} looks great on you")
-
-#2 The Age Gap:
-age=int(input("enter your age"))
-current_year=int(input("enter your current year"))
-future_age=age+(2050-current_year)
-print(f"In 2050 you will be {future_age} years old")
-
-#3 The Multiplier:
-num1=int(input("enter your first number"))
-num2=int(input("enter your second number"))
-print(num1*num2)
-#TypeError: can't multiply sequence by non-int of type 'str'
-#4 The Repeater:
-word=input("enter your word")
-num=int(input("enter your number"))
-print(num*word)
-#5 The Bill Splitter:
-totalbill=int(input("enter total bill of amount"))
-people=int(input("enter number of people"))
-print(f"{totalbill/people}")
-
-
-#3. Operators
-# 1The Remainder:
-num1=int(input("enter your first number"))
-num2=int(input("enter your second number"))
-print(f"number one:{num1} and number two: {num2}and its Remainder:{num1%num2} ")
-
-#2. The Power:
-x=int(input("enter your first number"))
-y=int(input("enter your second number"))
-print(f"{x**y}")
-
-#3. The Floor:
-num1=int(input("enter your first number"))
-num2=int(input("enter your second number"))
-print(f"{num1//num2}")
-
-#4. The Comparison:
-num = int(input("Enter a number: "))
-print(num > 100)
-
-#5.The Swapper:
-a=10
-b=15
-a,b=a,b
-print("a=",a)
-print("b=",b)
-
+import pandas as pd
+while True:
+    choise= input("Enter your choice:"
+                   "\n 1:create account: "
+                   "\n 2:check  balance:"
+                   "\n 3:withDraw amount"
+                   "\n 4:Deposit amount"
+                  "\n 5:Exist"
+                   )
+    if choise == "1":
+        print("create account")
+    elif choise == "2":
+        print("check balance")
+    elif choise == "3":
+        print("withdraw amount")
+    elif choise == "4":
+        print("deposit amount")
+    elif choise == "5":
+        print("exist")
+        break
+    useracc={"name":["umar"],"account":[103],"balance":[5000,]}
+    df=pd.DataFrame(useracc)
+    print(df)
 
